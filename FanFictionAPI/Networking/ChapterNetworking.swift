ed59d8b22220609baef824d972eef287fce3f9be
Natasha Martinez <nem4265@g.rit.edu>
2025-03-01 11:37:50 -0500

    Create a chapter networking class to keep track of the chapter loading

---
+++ FanFictionAPI/Networking/ChapterNetworking.swift
@@ -0,0 +1,62 @@
+//
+//  ChapterNetworking.swift
+//  FanFictionAPI
+//
+//  Created by Natasha Martinez on 3/1/25.
+//
+
+class ChapterNetworking {
+    /// Keeps track of the works loaded into memory
+    var worksCache: [String: Work] = [:]
+    
+    /// Fetches the work at a given chapter
+    /// - Parameters:
+    ///   - workID: the unique identifier for the work
+    ///   - chapterIndex: chapter index; defaults to 0
+    /// - Returns: the next chapter in the work
+    public func fetch(work workID: String, at chapterIndex: Int = 0) async -> Chapter? {
+        let work: Work
+        if let worksCached = worksCache[workID] {
+            work = worksCached
+        } else {
+            work = Work(id: workID, currentChapterIndex: chapterIndex)
+        }
+        
+        var chapterID: String? = nil
+        if chapterIndex > 0 {
+            let chapterListResult: Result<ChapterList?, Error> = await Networking.fetch(.workChapters(workID: workID))
+            switch chapterListResult {
+                case .success(let chapterList):
+                    work.chapterList = chapterList
+                    if work.currentChapterIndex >= work.chapterList!.chapters.count {
+                        chapterID = work.chapterList?.chapters[work.currentChapterIndex].url
+                    }
+                case .failure:
+                    break
+            }
+        }
+        
+        let workResult: Result<Work?, Error> = await Networking.fetch(.work(work: work, chapterID: chapterID))
+        switch workResult {
+            case .success(let work):
+                worksCache[workID] = work
+            case .failure:
+                break
+        }
+        
+        return worksCache[workID]?.currentChapter
+    }
+    
+    /// Fetches the next chapter in a given work. If work is not loaded yet, load first chapter
+    ///   - workID:
+    /// - Parameter workID: the unique identifier for the work
+    /// - Returns: the next chapter in the work
+    public func fetchNextChapter(for workID: String) async -> Chapter? {
+        let workResult: Result<Work?, Error>
+        if let work = worksCache[workID] {
+            return await fetch(work: workID, at: work.currentChapterIndex + 1)
+        } else {
+            return await fetch(work: workID)
+        }
+    }
+}
--- FanFictionAPI/State/Work.swift
+++ FanFictionAPI/State/Work.swift
@@ -6,28 +6,41 @@
 //
 
 public class Work {
+    /// The identifying number for this work
     let id: String
-    private var currentChapterIndex: Int
     
+    /// The current chapter index for use in chapterList
+    var currentChapterIndex: Int
+    
+    /// Returns the current chapter the user is on
     var currentChapter: Chapter? {
         guard currentChapterIndex < chapters.count else { return nil }
         return chapters[currentChapterIndex]
     }
     
-    var storyInfo: StoryInfo?
-    var aboutInfo: AboutInfo?
+    /// A list of chapter names and IDs for the given work
     var chapterList: ChapterList?
+    
+    /// A dict of chapters that have been loaded
+    var chapters: [Int: Chapter] = [:]
+    
+    /// Information about the fandom, relationships, etc
+    var storyInfo: StoryInfo?
+    
+    /// Information about the work's title, author, etc
+    var aboutInfo: AboutInfo?
+    
+    /// If vital information is missing
     var shouldParseAdditionalInfo: Bool {
         storyInfo == nil || aboutInfo == nil
     }
-    
-    var chapters: [Int: Chapter] = [:]
     
     public init(id: String, currentChapterIndex: Int = 0) {
         self.id = id
         self.currentChapterIndex = currentChapterIndex
     }
     
+    /// Saves a chapter at the current chapter index
     internal func saveAsCurrentChapter(_ chapter: Chapter) {
         chapters[currentChapterIndex] = chapter
     }
