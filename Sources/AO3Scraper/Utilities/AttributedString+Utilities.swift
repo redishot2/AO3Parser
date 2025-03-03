b6a300441e298c6f0336e405c4df6255041a81dc
Natasha Martinez <nem4265@g.rit.edu>
2025-03-03 18:39:48 -0500

    publicccc

--- Sources/AO3Scraper/Utilities/AttributedString+Utilities.swift
+++ Sources/AO3Scraper/Utilities/AttributedString+Utilities.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 extension AttributedString {
     
-    func convertToView() -> some View {
+    public func convertToView() -> some View {
         var textUI = Text("")
         var alignment: Alignment = .leading
         
